59fdc25c661be4e8b5240a6faa36a17acc9e93ea
Huguenin-Elie Steve <steve@huguenin-elie@protonmail.ch>
2020-10-05 14:51:23 +0200

    set asynchronous execution

--- server/app.js
+++ server/app.js
@@ -2,26 +2,32 @@ const WebSocket = require('ws')
 
 const wss = new WebSocket.Server({port: 3000})
 
-const { Database } = require('./database')
-const { Content } = require('./content')
+const { Database } = require('./database');
+const { Content } = require('./content');
 
-wss.on('connection', async (ws) => {
-    await ws.on('newsletter', async (user) => {
-        console.log('received user data')
-        
-        // serizalize data from newsletter
-        await Database.connect(async (client, dbName) => {
-            
+(async () => {
+    try {
+        await wss.on('connection', async (ws) => {
+            await ws.on('newsletter', async (user) => {
+                console.log('received user data')
+                
+                // serizalize data from newsletter
+                await Database.connect(async (client, dbName) => {
+                    
+                })
+
+                ws.emit('response', 'user data has been serialized')
+            })
+
+            await ws.on('translation', async (lang) => {
+                let content = await Content.translate(lang)
+                ws.emit('content', content)
+                ws.emit('response', 'text has been translated')
+            })
+
+            await ws.on('disconnect', () => {})
         })
-
-        ws.emit('response', 'user data has been serialized')
-    })
-
-    await ws.on('translation', async (lang) => {
-        let content = await Content.translate(lang)
-        ws.emit('content', content)
-        ws.emit('response', 'text has been translated')
-    })
-
-    await ws.on('disconnect', () => {})
+    } catch (e) {
+        console.log(e)
+    }
 })
