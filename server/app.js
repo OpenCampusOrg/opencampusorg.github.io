a170410063e58d72b01cb661bb12ee40721de0ce
Huguenin-Elie Steve <steve@huguenin-elie@protonmail.ch>
2020-10-05 14:51:37 +0200

    async and await were out of context

--- server/app.js
+++ server/app.js
@@ -42,49 +42,43 @@ const server = spdy.createServer({
 
 const wss = new WebSocket.Server({server});
 
-(async () => {
-    try {
-        await wss.on('connection', async (ws) => {
-            console.log('client connected through websocket')
+wss.on('connection', (ws) => {
+    console.log('client connected through websocket')
 
-            await ws.on('message', async (message) => {
-                console.log('Message received')
-                let data = JSON.parse(message)
-                if (data.handler == 'newsletter') {
-                    console.log('User data received')
-                    // serizalize data from newsletter
-                    await Database.connect(async (client, dbName) => {
-                    })
-                    await ws.send('user data has been serialized')
-                } else if (data.handler == 'translation') {
-                    let content = Content.translate(data.lang)
-                    content.handler = 'content'
-                    await ws.send(content)
-                    await ws.send('text has been translated')
-                } else {
-                    console.log('Message:', message)
-                }
+    ws.on('message', (message) => {
+        console.log('Message received')
+        let data = JSON.parse(message)
+        if (data.handler == 'newsletter') {
+            console.log('User data received')
+            // serizalize data from newsletter
+            Database.connect((client, dbName) => {
             })
+            ws.send('user data has been serialized')
+        } else if (data.handler == 'translation') {
+            let content = Content.translate(data.lang)
+            content.handler = 'content'
+            ws.send(JSON.stringify(content))
+            ws.send('text has been translated')
+        } else {
+            console.log('Message:', message)
+        }
+    })
 
-            await ws.on('error', (error) => {
-                console.error(error)
-            })
+    ws.on('error', (error) => {
+        console.error(error)
+    })
 
-            await ws.on('close', () => {
-                console.log('client disconnected from websocket')
-            })
-        })
+    ws.on('close', () => {
+        console.log('client disconnected from websocket')
+    })
+})
 
-        await wss.on('error', (error) => {
-            console.error(error)
-        })
+wss.on('error', (error) => {
+    console.error(error)
+})
 
-        await wss.on('close', () => {
-            console.log('closed websocket')
-        })
-    } catch (e) {
-        console.error(e)
-    }
+wss.on('close', () => {
+    console.log('closed websocket')
 })
 
 server.listen(3000, () => 
