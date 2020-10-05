101ade8471ef70d2687cce7fcd18666feaa147c4
Huguenin-Elie Steve <steve@huguenin-elie@protonmail.ch>
2020-10-05 14:51:29 +0200

    cleaned code and added missing await to ws.emit instruction

--- server/app.js
+++ server/app.js
@@ -11,27 +11,27 @@ const { Content } = require('./content');
         await wss.on('connection', async (ws) => {
             
             await ws.on('message', async (message) => {
-                console.log('message received')
+                console.log('Message received')
                 let data = JSON.parse(message)
                 if (data.handler == 'newsletter') {
-                    console.log('user data received')
+                    console.log('User data received')
                     // serizalize data from newsletter
                     await Database.connect(async (client, dbName) => {
                     })
-                    ws.emit('message', 'user data has been serialized')
+                    await ws.emit('message', 'user data has been serialized')
                 } else if (data.handler == 'translation') { 
                     let content = await Content.translate(lang)
                     content.handler = 'content'
                     await ws.emit('message', content)
                     await ws.emit('message', 'text has been translated')
                 } else {
-                    console.log('message:', message)
+                    console.log('Message:', message)
                 }
             })
 
             await ws.on('disconnect', () => {})
         })
     } catch (e) {
-        console.log(e)
+        console.error(e)
     }
 })
