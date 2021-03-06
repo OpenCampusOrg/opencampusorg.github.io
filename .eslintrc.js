e66c38f72fd94fa5e0adbda8e3781113cbbe50e2
Huguenin-Elie Steve <steve.huguenin-elie@protonmail.ch>
2021-03-06 22:58:27 +0100

    setting up backpack-core for running build

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,13 @@ module.exports = {
   ],
   plugins: [
   ],
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'build/webpack.base.conf.js'
+      }
+    }
+  },
   // add your custom rules here
   rules: {
     'nuxt/no-cjs-in-config': 0
