e6a167f4fbd08022420a945cb8dd61b8c5da2575
Huguenin-Elie Steve <steve.huguenin-elie@protonmail.ch>
2021-08-18 21:24:24 +0000

    prevent logging in production mode
    process.env were reset by former vue.config.js
    @babel/vue-preset-app is already in use

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,17 @@
 const npm_package = require('./package.json')
 
 process.env = {
+  ...process.env,
   VUE_APP_NAME: npm_package.name,
   VUE_APP_VERSION: npm_package.version,
   VUE_APP_DESCRIPTION: npm_package.description,
   VUE_APP_AUTHOR: npm_package.author,
   VUE_APP_URL: npm_package.url,
   VUE_APP_EMAIL: npm_package.email
+}
+
+module.exports = {
+  transpileDependencies: [
+    /mdb-vue-ui-kit/
+  ]
 }
