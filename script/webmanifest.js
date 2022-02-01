06422f5a92dc9d9b26a89cbbfef09c1ac312539b
Huguenin-Elie Steve <steve.huguenin-elie@protonmail.ch>
2022-02-01 17:44:22 +0000

    Revert "priorize using promises for fs operations"
    This reverts commit f2d9c02bff766691296e3ef9d6bd29009e3ad9d9.

--- script/webmanifest.js
+++ script/webmanifest.js
@@ -1,17 +1,17 @@
-const fs = require('fs/promise');
+const fs = require('fs');
 const path = require('path');
 
-const manifest_json = {
-  ...require('../dist/manifest.json'),
-  short_name: require('../package.json').short_name
-};
+const manifest_json = require('../dist/manifest.json');
+manifest_json.short_name = require('../package.json').short_name;
 
-await fs.writeFile(path.resolve(__dirname, '..', 'dist', 'manifest.json'), JSON.stringify(manifest_json), 'utf-8');
+fs.writeFileSync(path.resolve(__dirname, '..', 'dist', 'manifest.json'), JSON.stringify(manifest_json), 'utf-8');
 
-await fs.rename(path.resolve(__dirname, '..', 'dist', 'manifest.json'), path.resolve(__dirname, '..', 'dist', 'manifest.webmanifest'));
-console.log('Rename complete!');
+fs.rename(path.resolve(__dirname, '..', 'dist', 'manifest.json'), path.resolve(__dirname, '..', 'dist', 'manifest.webmanifest'), err => {
+  if (err) throw err;
+  console.log('Rename complete!');
+});
 
 const index_html = path.resolve(__dirname, '..', 'dist', 'index.html');
-const file = await fs.readFile(index_html);
+const file = fs.readFileSync(index_html);
 const lines = file.toString().replace(/(manifest).json/, '$1.webmanifest');
-await fs.writeFile(index_html, lines, 'utf-8');
+fs.writeFileSync(index_html, lines, 'utf-8');
